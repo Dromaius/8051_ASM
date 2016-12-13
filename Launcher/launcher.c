645469c142514b24706aa94239ccb977f10571c6
LayerDE <the_gamer@gmx.net>
2016-12-13 22:13:32 +0100

    launcher update

--- Launcher/launcher.c
+++ Launcher/launcher.c
@@ -4,7 +4,23 @@
 #include <stdbool.h>
 #include <string.h>
 #include <dlfcn.h>
-#include <iostream.h>
+#include <stdio.h>
+
+char Int2Char(uint8_t parse) 
+{
+    if(0 <=parse <= 9) return parse + '0';
+	else if(0x0A <= parse <= 0x0F) return parse + 'A' - 10;
+	else return 0;
+}
+
+uint8_t Char2Int(char hex)
+{
+	if( '0' <= hex <= '9') return hex - '0';
+	else if('A' <= hex <= 'F') return hex - 'A' + 10;
+	else if( 'a' <= hex <= 'f') return hex - 'a' + 10;
+	else return 255;
+}
 
 int main(){
+    printf("test\n");
 }
