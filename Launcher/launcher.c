faa23f78cf360fa787d21888b4c8a15423962352
LayerDE <the_gamer@gmx.net>
2016-12-14 20:25:32 +0100

    (loadHEX_func)

--- Launcher/launcher.c
+++ Launcher/launcher.c
@@ -5,6 +5,9 @@
 #include <string.h>
 #include <dlfcn.h>
 #include <stdio.h>
+typedef bool (*loadHEX_func)(uint8_t*,char*);
+typedef void (*exec_func)();
+typedef void (*exec_func)();
 
 char Int2Char(uint8_t parse) 
 {
@@ -22,5 +25,15 @@ uint8_t Char2Int(char hex)
 }
 
 int main(){
-    printf("test\n");
+	uint8_t eeprom_test[64*1024];
+	uint8_t* test_eeprom=eeprom_test;
+	void* file_reader = dlopen("./file_reader.so",RTLD_LAZY);
+	printf("debug\n");
+	if(((loadHEX_func)dlsym(file_reader,"read_IntelHEX"))(test_eeprom,"../tests/test2.hex"))
+		printf("loading complete\n");
+	else
+		printf("loading failed");
+	while(test_eeprom+64*1024-(test_eeprom++)) 
+		if(*(test_eeprom)) {printf(Char2Int(*test_eeprom&0xF0));printf(Char2Int(*test_eeprom&0x0F));};
+	return 0;
 }
